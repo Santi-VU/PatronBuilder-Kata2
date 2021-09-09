2c219ee781e0177fae766135f1bd6ae8016f6ff5
Santi-VU <89940984+Santi-VU@users.noreply.github.com>
2021-09-09 15:21:24 +0200

    Add files via upload

---
+++ src/PatronBuilder-Kata2/Models/CoffeeBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronBuilder_Kata2.Models
+{
+	class CoffeeBuilder
+	{
+		private string _typeCoffe;
+		private List<Milk> _milkList;
+		private List<Sugar> _sugarList;
+
+		public CoffeeBuilder() {
+			this._milkList = new List<Milk>();
+			this._sugarList = new List<Sugar>();
+		}
+
+		public CoffeeBuilder SetBlackCoffee() {
+			this._typeCoffe = "Black";
+			return this;
+		}
+		public CoffeeBuilder SetCubanoCoffee() {
+			this._typeCoffe = "Cubano";
+			return this; 
+		}
+		public CoffeeBuilder SetAntoccinoCoffee() {
+			this._typeCoffe = "Americano";
+			return this; 
+		}
+
+		public CoffeeBuilder WithMilk(double fat) {
+			this._milkList.Add(new Milk(fat));
+			return this; 
+		}
+		public CoffeeBuilder WithSugar(string sort) {
+			this._sugarList.Add(new Sugar(sort));
+			return this; 
+		}
+
+		public Coffee Build() { return new Coffee(this._typeCoffe, this._milkList, this._sugarList); }
+	};
+}
---
+++ src/PatronBuilder-Kata2/Program.cs
@@ -0,0 +1,73 @@
+﻿using PatronBuilder_Kata2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PatronBuilder_Kata2
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine(@"
+					To run tests write:
+					testAll
+					test1
+					test2
+					test3
+					");
+
+				var instrucction = Console.ReadLine();
+				if (instrucction == "testAll")
+				{
+					SampleTests.Test1();
+					SampleTests.Test2();
+					SampleTests.Test3();
+				}
+				else if (instrucction == "test1")
+					SampleTests.Test1();
+				else if (instrucction == "test2")
+					SampleTests.Test2();
+				else if (instrucction == "test3")
+					SampleTests.Test3();
+				}
+		}
+	}
+	public static class SampleTests
+	{
+		public static void Test1()
+		{
+			var actual = new CoffeeBuilder().SetBlackCoffee().WithSugar("Regular").WithMilk(3.2).Build();
+			var expected = new Coffee("Black", new List<Milk> { new Milk(3.2) }, new List<Sugar> { new Sugar("Regular") });
+			Console.WriteLine($"expected: {expected}");
+			Console.WriteLine($"actual: {actual}");
+
+			bool assertAreEqual = expected.ToString() == actual.ToString();
+			Console.WriteLine("Test " + (assertAreEqual ? " -------- ok" : " -------------- FAIL"));
+		}
+
+		public static void Test2()
+		{
+			var actual = new CoffeeBuilder().SetAntoccinoCoffee().WithMilk(0.5).Build();
+			var expected = new Coffee("Americano", new List<Milk> { new Milk(0.5) }, new List<Sugar>());
+			Console.WriteLine($"expected: {expected}");
+			Console.WriteLine($"actual: {actual}");
+
+			bool assertAreEqual = expected.ToString() == actual.ToString();
+			Console.WriteLine("Test " + (assertAreEqual ? " -------- ok" : " -------------- FAIL"));
+		}
+
+		public static void Test3()
+		{
+			var actual = new CoffeeBuilder().SetCubanoCoffee().WithSugar("Brown").Build();
+			var expected = new Coffee("Cubano", new List<Milk>(), new List<Sugar> { new Sugar("Brown") });
+			Console.WriteLine($"expected: {expected}");
+			Console.WriteLine($"actual: {actual}");
+
+			bool assertAreEqual = expected.ToString() == actual.ToString();
+			Console.WriteLine("Test " + (assertAreEqual ? " -------- ok" : " -------------- FAIL"));
+		}
+	}
+}
